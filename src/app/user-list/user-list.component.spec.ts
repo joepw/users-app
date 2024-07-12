a6f3c70d5509b97a2e187eee9e2485522f839848
joepw <jonathanpwijaya@gmail.com>
2024-07-12 15:30:32 +0700

    add sort and search feature

--- src/app/user-list/user-list.component.spec.ts
+++ src/app/user-list/user-list.component.spec.ts
@@ -22,7 +22,10 @@ describe('UserListComponent', () => {
     })
     .compileComponents();
     userServiceSpy = TestBed.inject(UsersService) as jasmine.SpyObj<UsersService>;
-    userServiceSpy.getAllUsers.and.resolveTo([{ id: 1, name: 'Budi' } as User]);
+    userServiceSpy.getAllUsers.and.resolveTo([
+      { id: 1, name: 'Budi', email: 'budi@budi.com', website: 'budi.com' } as User,
+      { id: 2, name: 'Adi', email: 'adi@a.com', website: 'adi.com' } as User]
+    );
 
     fixture = TestBed.createComponent(UserListComponent);
     component = fixture.componentInstance;
@@ -65,12 +68,58 @@ describe('UserListComponent', () => {
     expect(component.userId).toBe('1');
   }))
 
-  // it('should show no data on empty result', fakeAsync(() => {    
-  //   mockValue = [];
-  //   fixture = TestBed.createComponent(UserListComponent);
-  //   fixture.detectChanges();
-  //   tick();
-  //   const noData: HTMLElement = fixture.debugElement.nativeElement.querySelector('td');
-  //   expect(noData.textContent).toEqual('No Data');
-  // }));
+  it('should sort table correctly', fakeAsync(() => {    
+    tick();
+    fixture.detectChanges();
+    let name: HTMLElement[] = fixture.debugElement.nativeElement.querySelectorAll('.name > span');
+    expect(name[0].textContent).toEqual('Budi');
+    const headers: HTMLElement[] = fixture.debugElement.nativeElement.querySelectorAll('.head-wrapper');
+    const nameHeader = headers[0];
+    nameHeader.click();
+    fixture.detectChanges();
+    name = fixture.debugElement.nativeElement.querySelectorAll('.name > span');
+    expect(component.sortKey).toBe('name');
+    expect(component.sortDirection).toBe('ASC');
+    expect(name[0].textContent).toEqual('Adi');
+  
+    nameHeader.click();
+    fixture.detectChanges();
+    name = fixture.debugElement.nativeElement.querySelectorAll('.name > span');
+    expect(component.sortKey).toBe('name');
+    expect(component.sortDirection).toBe('DESC');
+    expect(name[0].textContent).toEqual('Budi');
+
+    nameHeader.click();
+    fixture.detectChanges();
+    name = fixture.debugElement.nativeElement.querySelectorAll('.name > span');
+    expect(component.sortKey).toBe('id');
+    expect(component.sortDirection).toBe('');
+    expect(name[0].textContent).toEqual('Budi');
+  }));
+
+  it('should sort user list correctly', fakeAsync(async () => {    
+    tick();
+    fixture.detectChanges();
+    const input: HTMLInputElement = fixture.debugElement.nativeElement.querySelector('input');
+    input.value = 'adi';
+    input.dispatchEvent(new Event('input'));
+    fixture.detectChanges();
+    expect(component.searchKeyword).toBe('adi');
+    let name = fixture.debugElement.nativeElement.querySelector('.name > span');
+    expect(name.textContent).toEqual('Adi');
+
+    input.value = '';
+    input.dispatchEvent(new Event('input'));
+    fixture.detectChanges();
+    expect(component.searchKeyword).toBe('');
+    name = fixture.debugElement.nativeElement.querySelector('.name > span');
+    expect(name.textContent).toEqual('Budi');
+
+    input.value = 'asdf';
+    input.dispatchEvent(new Event('input'));
+    fixture.detectChanges();
+    expect(component.searchKeyword).toBe('asdf');
+    name = fixture.debugElement.nativeElement.querySelector('.name > span');
+    expect(name).toBeNull();
+  }));
 });
--- src/app/user-list/user-list.component.ts
+++ src/app/user-list/user-list.component.ts
@@ -5,6 +5,7 @@ import { RouterLink, Router, NavigationEnd } from '@angular/router';
 import { UserDetailComponent } from '../user-detail/user-detail.component';
 import { UsersService } from '../users.service';
 import { User } from '../user';
+import { SORT_DIRECTION } from './user-list.component.constants';
 
 @Component({
   selector: 'app-user-list',
@@ -18,6 +19,11 @@ export class UserListComponent {
   userList: User[] = [];
   userId = '';
   isLoading = true;
+  sortKey: keyof User = 'id';
+  sortDirection: typeof SORT_DIRECTION[number] = '';
+  searchKeyword = '';
+  filteredUserList: User[] = [];
+
 
   constructor(private router: Router, private location: Location) {
     router.events.pipe(filter(event => event instanceof NavigationEnd)).subscribe(() => this.checkPath());
@@ -37,5 +43,39 @@ export class UserListComponent {
 
   onClickUser(id: number) {
     this.router.navigate(['/users', id]);
+  }
+
+  sortData(key: keyof User) {
+    if (this.sortKey === key) {
+      let newSortDirectionIndex = SORT_DIRECTION.indexOf(this.sortDirection) + 1;
+      this.sortDirection = SORT_DIRECTION[newSortDirectionIndex];
+      if (!this.sortDirection) {
+        this.sortKey = 'id';
+      }
+    } else {
+      this.sortKey = key;
+      this.sortDirection = SORT_DIRECTION[0];
+    }
+    this.userList.sort((userA, userB) => {
+      let result = 0;
+      if (userA[this.sortKey] < userB[this.sortKey]) {
+        result = -1;
+      }
+      if (userA[this.sortKey] > userB[this.sortKey]) {
+        result = 1;
+      }
+      if (this.sortDirection === 'DESC') result *= -1;
+      return result;
+    })
+  }
+
+  onInputChange($event: Event) {
+    const inputValue = ($event.target as HTMLInputElement).value;
+    this.searchKeyword = inputValue.toLowerCase();
+    this.filteredUserList = this.userList.filter(user => {
+      return user.name.toLowerCase().includes(this.searchKeyword)
+        || user.email.toLowerCase().includes(this.searchKeyword)
+        || user.website.toLowerCase().includes(this.searchKeyword);
+    })
   }
 }
